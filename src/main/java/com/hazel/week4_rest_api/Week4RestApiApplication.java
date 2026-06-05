c04b55c7d26741c574911e702089856da80f23a2
hyejun <hyejunkkim228@gmail.com>
2026-06-05 15:38:55 +0900

    first commit

---
+++ src/main/java/com/hazel/week4_rest_api/Week4RestApiApplication.java
@@ -0,0 +1,13 @@
+package com.hazel.week4_rest_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week4RestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Week4RestApiApplication.class, args);
+	}
+
+}
